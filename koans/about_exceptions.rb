a4eb5f0a7549eae2a9019d448ead84576259bbc3
Vaibhav Singh <vaibhavsingh@Vaibhavs-MacBook-Pro.local>
2019-05-21 18:05:47 +0530

    completed "about exceptions"

--- koans/about_exceptions.rb
+++ koans/about_exceptions.rb
@@ -1,15 +1,15 @@
-require 'edgecase'
+require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-class AboutExceptions < EdgeCase::Koan
+class AboutExceptions < Neo::Koan
 
   class MySpecialError < RuntimeError
   end
 
   def test_exceptions_inherit_from_Exception
-    assert MySpecialError.ancestors.include?(RuntimeError)
-    assert MySpecialError.ancestors.include?(StandardError)
-    assert MySpecialError.ancestors.include?(Exception)
-    assert MySpecialError.ancestors.include?(Object)
+    assert_equal RuntimeError, MySpecialError.ancestors[1]
+    assert_equal StandardError, MySpecialError.ancestors[2]
+    assert_equal Exception, MySpecialError.ancestors[3]
+    assert_equal Object, MySpecialError.ancestors[4]
   end
 
   def test_rescue_clause
@@ -20,15 +20,15 @@ class AboutExceptions < EdgeCase::Koan
       result = :exception_handled
     end
 
-    assert_equal __, result
+    assert_equal :exception_handled, result
 
-    assert ex.is_a?(StandardError), "Failure message."
-    assert ex.is_a?(RuntimeError), "Failure message."
+    assert_equal true, ex.is_a?(StandardError), "Should be a Standard Error"
+    assert_equal true, ex.is_a?(RuntimeError),  "Should be a Runtime Error"
 
     assert RuntimeError.ancestors.include?(StandardError),
       "RuntimeError is a subclass of StandardError"
-    
-    assert_equal __, ex.message
+
+    assert_equal "Oops", ex.message
   end
 
   def test_raising_a_particular_error
@@ -40,21 +40,29 @@ class AboutExceptions < EdgeCase::Koan
       result = :exception_handled
     end
 
-    assert_equal __(:exception_handled), result
-    assert_equal __, ex.message
+    assert_equal :exception_handled, result
+    assert_equal "My Message", ex.message
   end
 
   def test_ensure_clause
     result = nil
     begin
       fail "Oops"
-    rescue StandardError => ex
+    rescue StandardError
       # no code here
     ensure
       result = :always_run
     end
 
-    assert_equal __, result
+    assert_equal :always_run, result
+  end
+
+  # Sometimes, we must know about the unknown
+  def test_asserting_an_error_is_raised
+    # A do-end is a block, a topic to explore more later
+    assert_raise(RuntimeError) do
+      raise MySpecialError.new("New instances can be raised directly.")
+    end
   end
 
 end
