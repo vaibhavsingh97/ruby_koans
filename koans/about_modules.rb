2f4e72b03418c61c5584c602740e487952407eea
Vaibhav Singh <vaibhavsingh@Vaibhavs-MacBook-Pro.local>
2019-05-22 15:02:49 +0530

    completed "About module"

--- koans/about_modules.rb
+++ koans/about_modules.rb
@@ -1,6 +1,6 @@
-require 'edgecase'
+require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-class AboutModules < EdgeCase::Koan
+class AboutModules < Neo::Koan
   module Nameable
     def set_name(new_name)
       @name = new_name
@@ -12,7 +12,7 @@ class AboutModules < EdgeCase::Koan
   end
 
   def test_cant_instantiate_modules
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       Nameable.new
     end
   end
@@ -39,25 +39,25 @@ class AboutModules < EdgeCase::Koan
 
   def test_normal_methods_are_available_in_the_object
     fido = Dog.new
-    assert_equal __, fido.bark
+    assert_equal "WOOF", fido.bark
   end
 
-  def test_module_methods_are_also_availble_in_the_object
+  def test_module_methods_are_also_available_in_the_object
     fido = Dog.new
-    assert_nothing_raised(Exception) do
-      fido.set_name("Rover")      
+    assert_nothing_raised do
+      fido.set_name("Rover")
     end
   end
 
   def test_module_methods_can_affect_instance_variables_in_the_object
     fido = Dog.new
-    assert_equal __, fido.name
+    assert_equal "Fido", fido.name
     fido.set_name("Rover")
-    assert_equal __, fido.name
+    assert_equal "Rover", fido.name
   end
 
   def test_classes_can_override_module_methods
     fido = Dog.new
-    assert_equal __, fido.here
+    assert_equal :in_object, fido.here
   end
 end
